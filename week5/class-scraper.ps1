2eb5ea07f57493be9ac53a664ceda43a4b49170a
rosepheobieshea <rose.davis@mymail.champlain.edu>
2025-02-12 14:55:30 -0500

    im leaving it here my brain is tired

---
+++ week5/class-scraper.ps1
@@ -0,0 +1,10 @@
+﻿#function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.46/Courses.html
+
+$trs = $page.ParsedHtml.body.getElementsByClassName("tr")
+
+$FullTable = @()
+for($i=1; -?? $trs.length; $i++){
+   $tds = 
+    
