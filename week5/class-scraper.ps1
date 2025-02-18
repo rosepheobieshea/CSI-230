0338fd87a7a74dd1665afd4ee73885410cd21c4c
rosepheobieshea <rose.davis@mymail.champlain.edu>
2025-02-18 16:15:20 -0500

    week5 finality

--- week5/class-scraper.ps1
+++ week5/class-scraper.ps1
@@ -1,10 +1,47 @@
-﻿#function gatherClasses(){
+﻿function gatherClasses(){
 
 $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.46/Courses.html
 
-$trs = $page.ParsedHtml.body.getElementsByClassName("tr")
+$trs = $page.ParsedHtml.body.getElementsByTagName("tr")
 
 $FullTable = @()
-for($i=1; -?? $trs.length; $i++){
-   $tds = 
-    
+
+for($i=1; $i -lt $trs.length; $i++){
+   $tds = $trs[$i].getElementsByTagName("td")
+   $Times = $tds[5].innerText.Split("-")
+
+   $FullTable += [PSCustomObject]@{"Class Code" = $tds[0].innerText
+                                   "Title" = $tds[1].innerText
+                                   "Days" = $tds[4].innerText
+                                   "Time Start" = $Times[0]
+                                   "Time End" = $Times[1]
+                                   "Instructor" = $tds[6].innerText
+                                   "Location" = $tds[9].innerText
+                                   }
+}
+$FullTable = daysTranslator $FullTable
+return $FullTable
+}
+
+
+function daysTranslator($FullTable){
+
+for($i=0; $i -lt $FullTable.length; $i++){
+    $Days = @()
+
+    if($FullTable[$i].Days -ilike "*M*"){ $Days += "Monday" }
+
+    if($FullTable[$i].Days -ilike "*[TF]*"){ $Days += "Tuesday" }
+    elseif($FullTable[$i].Days -ilike "*T*"){ $Days += "Tuesday" }
+
+    if($FullTable[$i].Days -ilike "*W*"){ $Days += "Wednesday" }
+
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thursday" }
+
+    if($FullTable[$i].Days -ilike "*F*"){ $Days += "Friday" }
+
+    $FullTable[$i].Days = $Days
+
+}
+return $FullTable
+}
---
+++ week5/scraper-main.ps1
@@ -0,0 +1,15 @@
+﻿. (Join-Path $PSScriptRoot class-scraper.ps1)
+
+$allClasses = gatherClasses
+
+$ITSInstructors = $allClasses | Where-Object { ($_."Class Code" -ilike "SYS*") -or
+                                               ($_."Class Code" -ilike "NET*") -or
+                                               ($_."Class Code" -ilike "SEC*") -or
+                                               ($_."Class Code" -ilike "FOR*") -or
+                                               ($_."Class Code" -ilike "CSI*") -or
+                                               ($_."Class Code" -ilike "DAT*") }
+
+#$ITSInstructors | Sort-Object "Instructor" -Unique |  Format-Table "Instructor"
+
+$allClasses | Group-Object { $_.Instructor -in $ITSInstructors.Instructor } | Select-Object Count, Name `
+ | Sort-Object Count -Descending
