bf1b1a573ff949ac2982434aab8a0b826020a290
rosepheobieshea <rose.davis@mymail.champlain.edu>
2025-02-19 12:33:26 -0500

    initial commit for week6

---
+++ week4/Untitled5.ps1
@@ -0,0 +1,7 @@
+﻿cls
+$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.46/ToBeScraped.html
+
+$divs1= $scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+ $_.getAttributeNode("class").Value -ilike "*div-1*"} | select innerText
+
+$divs1
