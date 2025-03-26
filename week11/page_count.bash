45b04628423d3672b64f59e2fbe20247e85786f3
rosepheobieshea <rose.davis@mymail.champlain.edu>
2025-03-26 13:47:28 -0400

    hw finished !

--- week11/page_count.bash
+++ week11/page_count.bash
@@ -2,7 +2,6 @@
 
 function page_count()
 {
-
 file="/var/log/apache2/access.log"
 pages=$(cat "$file" | cut -d' ' -f7)
 
@@ -12,7 +11,14 @@ page2_count=$(echo $pages | grep -o "/page2.html" | wc -l)
 root_count=$(echo $pages | grep -o "/" | wc -l)
 #echo "$pages"
 printf " $root_count / \n $index_count /index.html \n $page1_count /page1.html \n $page2_count /page2.html \n "
-
 }
 
-page_count
+function counting_curl_access()
+{
+file="/var/log/apache2/access.log"
+access_count=$(cat "$file" | cut -d' ' -f1,12 | grep "curl" | wc -l)
+access_name=$(cat "$file" | cut -d' ' -f1,12 | grep "curl" | uniq)
+printf "$access_count $access_name \n"
+}
+
+counting_curl_access
