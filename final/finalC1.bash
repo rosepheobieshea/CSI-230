14082151e5768010b7d44242fa70a6902e8338c6
rosepheobieshea <rose.davis@mymail.champlain.edu>
2025-04-30 13:04:14 -0400

    final challenge 1 complete

---
+++ final/finalC1.bash
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# This is the link we will scrape
+link="10.0.17.6/IOC.html"
+
+# get it with curl and tell curl not to give errors
+fullPage=$(curl -sL "$link")
+
+# Utilizing xmlstarlet tool to extract table from the page
+toolOutput=$(echo "$fullPage" | \
+xmlstarlet format --html --recover 2>/dev/null | \
+xmlstarlet select --template --copy-of \
+"//html//body//table//tr//td[1]")
+
+# Processing HTML with sed
+# 1- Replacing every </tr> with a line break
+echo "$toolOutput" | sed 's/<\/td>/\n/g' | sed 's/<td>//g' > IOC.txt
